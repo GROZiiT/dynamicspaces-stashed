9447984d4372a212a227ddea556a5889dc4e69ab
goutham <30994768+MGouthamB@users.noreply.github.com>
2023-07-09 10:48:23 +0100

    payments
    1.0

--- App/models.py
+++ App/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+import time
+from ckeditor.fields import RichTextField
+from datetime import datetime
 # Create your models here.
+class Jobs(models.Model):
+    title= models.CharField(max_length=500)
+    keywords=models.CharField(max_length=500)
+    company=models.CharField(max_length=500,default="")
+    sdescription=models.CharField(max_length=500)
+    description=RichTextField()
+    background_img_url = models.CharField(max_length=500)
+    logo_img_url = models.CharField(max_length=500)
+    location=models.CharField(max_length=500)
+    eemail = models.CharField(max_length=500)
+    expire_in_days=models.DateField()
+    time=models.DateField(editable=False,default=datetime.now())
+    posted_by=models.CharField(max_length=500)
+
+class Profiles(models.Model):
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500,unique=True)
+    username = models.CharField(max_length=500)
+    password = models.CharField(max_length=500)
+    comapny = models.CharField(max_length=500)
+    job = models.CharField(max_length=500)
+    email_verfied = models.BooleanField(default=False)
+    subscriber = models.BooleanField(default=False)
+    subscriber_id = models.CharField(max_length=500,default="")
+    key = models.CharField(max_length=500)
+    img_url = models.CharField(max_length=500)
+    profile_pic = models.ImageField(upload_to='images')
--- App/views.py
+++ App/views.py
@@ -1,6 +1,103 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+from .models import *
+from cryptography.fernet import Fernet
+import random
+import datetime
+import requests
+from django import template
 
+register = template.Library()
+client_id = 'AZbjl24UxpurpCWivsPxyAYCbHNkoTgoPxMcHXp0vlEWItKQ_s1PY9YTAXqk540STaqwzKrzihlMCnw0'
+client_key = 'EDXoiu_RvG2otCBFGTHbfM2z08LnGLzEKGFpFQt22HpS2RWG1ADutj9PIJ-xCWnrBe-ynERD_stCPAP_'
+own_key = 'LionTrades_Subscription'
+
+def get_access_token():
+    data = {
+        'grant_type': 'client_credentials',
+    }
+
+    response = requests.post('https://api-m.sandbox.paypal.com/v1/oauth2/token', data=data, auth=(
+        client_id,client_key))
+    return response.json()["access_token"]
+
+def payment(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    try:
+        user = Jobs.objects.get(username=request.session['email'])
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + get_access_token(),
+        }
+
+        subscriptions_response = requests.get(
+            'https://api-m.sandbox.paypal.com/v1/billing/subscriptions/' + request.POST['subscriptionID'],
+            headers=headers)
+
+        user.subscriber_id = request.POST['subscriptionID']
+        user.subscriber = True
+        user.save()
+
+        request.session['message'] = 'Payment Successful..!'
+        return HttpResponseRedirect("users-profile")
+
+    except Exception as e:
+        request.session['message'] = 'Payment Unsuccessful..!Please contact admin or try again.'
+        return HttpResponseRedirect("users-profile")
+
+@register.simple_tag
+def split(string):
+    return string.split()
+
+def logged_in(request):
+    try:
+        profile = Profiles.objects.get(email=request.session['email'])
+        fernet = Fernet(profile.key.encode('utf-8'))
+        if(profile.password == fernet.decrypt(request.session['password']).decode()):
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
+def register(request):
+    try:
+        profile = Profiles()
+        profile.name= request.POST['name']
+        profile.email = request.POST['email']
+        profile.username = request.POST['username']
+        profile.password = request.POST['password']
+        profile.job = request.POST['job']
+        profile.name = request.POST['name']
+        profile.key = Fernet.generate_key().decode('utf-8')
+        profile.save()
+        return HttpResponseRedirect('pages-login')
+    except Exception as e:
+        print(e)
+        return render(request, "pages-register.html")
+
+def login(request):
+    try:
+        profile = Profiles.objects.get(email=request.POST['email'])
+        if(profile.password == request.POST['password']):
+            request.session['email']= profile.email
+            fernet = Fernet(profile.key.encode('utf-8'))
+            request.session['password'] = fernet.encrypt(profile.password.encode()).decode('utf-8')
+            return HttpResponseRedirect('index')
+        else:
+            raise Exception
+    except Exception as e:
+        print(e)
+        request.session['message'] = "The Username or password is incorret, please try again"
+        return HttpResponseRedirect('pages-login')
+
+def logout(request):
+    try:
+        request.session.flush()
+    except:
+        pass
+    return HttpResponseRedirect('pages-login')
 
 def handler404(request, exception):
     return render(request, 'pages-error-404.html', status=404)
@@ -11,10 +108,52 @@ def handler500(request):
 
 
 def index(request):
-    return render(request, "index.html")
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+
+    jobs=Jobs.objects.filter(posted_by=request.session['email'])
+    return render(request, "index.html",{'jobs':jobs})
 
 def add_post(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
     return render(request, "add-post.html")
+
+def adding_job(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    try:
+        job =Jobs()
+        job.title=request.POST['title']
+        job.sdescription = request.POST['sdescription']
+        job.description = request.POST['description']
+        job.location = request.POST['location']
+        job.eemail = request.POST['eemail']
+        job.company = request.POST['company']
+        job.logo_img_url = request.POST['logo_img_url']
+        job.expire_in_days = request.POST['expire_in_days']
+        job.background_img_url = request.POST['background_img_url']
+        job.keywords = request.POST['keywords']
+        job.posted_by= request.session['email']
+        job.save()
+        return HttpResponseRedirect('index')
+    except Exception as e:
+        print(e)
+        return HttpResponseRedirect('add-post')
+
+
+def GroziitDynamicSpace(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    jobs = Jobs.objects.filter(posted_by=request.session['email'])
+    return render(request, "GroziitJobs.html",{'jobs':jobs})
+
+def postdetail(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    job = Jobs.objects.get(id=request.GET['job'],posted_by=request.session['email'])
+    # job.time = tim
+    return render(request, "detail.html",{"job":job})
 
 def charts_apexcharts(request):
     return render(request, "charts-apexcharts.html")
@@ -125,6 +264,13 @@ def pages_faq(request):
 
 
 def pages_login(request):
+    try:
+        if request.session["message"]!="":
+            message= request.session["message"]
+            request.session["message"] = ""
+            return render(request, "pages-login.html", {"message":message})
+    except:
+        pass
     return render(request, "pages-login.html")
 
 
@@ -141,4 +287,44 @@ def tables_general(request):
 
 
 def users_profile(request):
-    return render(request, "users-profile.html")
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    if "message" not in request.session.keys():
+        request.session["message"]=""
+    msg,request.session["message"]=request.session["message"],""
+    return render(request, "users-profile.html",{"msg":msg})
+
+def cancelsub(request):
+    if not logged_in(request):
+        return HttpResponseRedirect('pages-login')
+    try:
+        user = Profiles.objects.get(username=request.session['email'])
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + get_access_token(),
+        }
+
+        json_data = {
+            'reason': 'NA',
+        }
+
+        response = requests.post(
+            'https://api-m.sandbox.paypal.com/v1/billing/subscriptions/' + user.subscriber_id + '/cancel',
+            headers=headers,
+            json=json_data,
+        )
+
+        if response.status_code != 204:
+            raise Exception
+
+        user.subscriber = False
+        user.subscriber_id = ""
+        user.save()
+
+        request.session['message'] = 'Successfully cancelled Subscription'
+        return HttpResponseRedirect("users-profile")
+
+    except Exception as e:
+        request.session['message'] = 'There is an error cancelling the Subscription, please try again or Contact us'
+        return HttpResponseRedirect("users-profile")
